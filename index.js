7714b3249a7cc7fdb8e2fa585fcc54a211722557
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2025-01-09 22:29:13 -0700

    Update index.js
    included cors to allow requests from different domains
    added in input validators for new user account creation and for updating user info

--- index.js
+++ index.js
@@ -16,9 +16,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins then
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
+
+const { check, validationResult } = require('express-validator');
 
 // let users = [
 //     {
@@ -179,31 +195,48 @@ app.use(express.static('public'));
 
 
 //CREATE Add user
-app.post('/users', async (req, res) => {
-    await Users.findOne({ Username: req.body.Username})
-        .then((user) => {
-            if (user) {
-                return res.status(400).send(req.body.Username + 'already exists');
-            } else {
-                Users
-                    .create({
-                        Username: req.body.Username,
-                        Password: req.body.Password,
-                        Email: req.body.Email,
-                        Birthday: req.body.Birthday
-                    })
-                    .then((user) => {res.status(201).json(user)})
-                    .catch((error) => {
-                        console.error(error);
-                        res.status(500).send('Error: ' + error);
-                    })
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-        });
-});
+app.post('/users',
+    //Validation logic for request
+    [
+        check('Username', 'Username is required').isLength({min: 5}),
+        check('Username', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
+        check('Password', 'Password must be at least 8 characters long').isLength({min: 8}),
+        check('Email', 'Email does not appear to be valid').isEmail()
+    ],
+    async (req, res) => {
+
+        //check the validation object for errors
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()){
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        await Users.findOne({ Username: req.body.Username})
+            .then((user) => {
+                if (user) {
+                    return res.status(400).send(req.body.Username + 'already exists');
+                } else {
+                    Users
+                        .create({
+                            Username: req.body.Username,
+                            Password: hashedPassword,
+                            Email: req.body.Email,
+                            Birthday: req.body.Birthday
+                        })
+                        .then((user) => {res.status(201).json(user)})
+                        .catch((error) => {
+                            console.error(error);
+                            res.status(500).send('Error: ' + error);
+                        })
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error: ' + error);
+            });
+    });
 
 //Get all movies
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) =>{
@@ -282,27 +315,41 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', { sessio
 })
 
 //Update a user's info, by username
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    //Condition to check if user matches the profile they are trying to edit below
-    if(req.user.Username !== req.params.Username){
-        return res.status(400).send('Permission Denied');
-    }
-    await Users.findOneAndUpdate({ Username: req.params.Username}, { $set: 
-        {
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
+app.put('/users/:Username',
+    //Validation logic here for request
+    [
+        check('Username', 'Username is required').isLength({min: 5}),
+        check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+        check('Password', 'Password must be at least 8 characters.').isLength({min: 8}),
+        check('Email', 'Email does not appear to be valid.').isEmail()
+    ], passport.authenticate('jwt', { session: false }), async (req, res) => {
+        //Check the validation for errors
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
         }
-    },
-    {new: true})
-    .then((updatedUser) => {
-        res.json(updatedUser);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-    });
+        //Condition to check if user matches the profile they are trying to edit below
+        if(req.user.Username !== req.params.Username){
+            return res.status(400).send('Permission Denied');
+        }
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        await Users.findOneAndUpdate({ Username: req.params.Username}, { $set: 
+            {
+                Username: req.body.Username,
+                Password: hashedPassword,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            }
+        },
+        {new: true})
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Add a movie to a user's list of favorites
