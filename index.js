378033fb35cb431e1fb02623c9a4dd0cbf499624
carsonrb11 <155111205+carsonrb11@users.noreply.github.com>
2025-01-15 13:41:23 -0700

    Update index.js
    Updated greeting upon opening app

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'),
 app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my app!');
+    res.send('Welcome to Movie Madness!');
 });
 
 app.use(express.static('public'));
